6ff2cac0bc9d4a54050b8c19260e600276beabe9
Aym-fuhong <18829287015@163.com>
2016-06-01 22:16:37 +0800

    第四题

---
+++ src/collect_all_even.js
@@ -0,0 +1,13 @@
+
+function collect_same_elements(collection_a, object_b) {
+  var newarr = [];
+ for(var i =0;i<arr.length;i++){ 
+ 	for(var j=0;j<json.value.length;j++){
+   	    if(arr[i].key==json.value[j]){
+   	    	newarr.push(arr[i].key );
+   	    }
+   }
+ }
+}
+
+module.exports = collect_same_elements;
